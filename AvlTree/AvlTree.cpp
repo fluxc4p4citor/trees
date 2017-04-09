4c0caed7e87a825f0a1c433b90967e5971864c22
Michael Satran <michael.satran@outlook.com>
2017-04-09 14:26:47 -0700

    initial

--- AvlTree/AvlTree.cpp
+++ AvlTree/AvlTree.cpp
@@ -1,11 +1,32 @@
 // AvlTree.cpp : Defines the entry point for the console application.
 //
+#include "stdafx.h";
 
-#include "stdafx.h"
+#include<stdlib.h>
 
+typedef struct tree_node
+{
+	int balanceFactor;
+	struct tree_node* left;
+	struct tree_node* right;
+	int data;
+} Node;
+
+Node* Node_init()
+{
+	Node* node = (Node*)malloc(sizeof(Node));
+
+	node->balanceFactor = 0;
+	node->left = 0;
+	node->right = 0;
+	node->data = 0;
+
+	return node;
+}
 
 int main()
 {
-    return 0;
+	Node* test = Node_init();
+	printf("");
 }
 
